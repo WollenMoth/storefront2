b6ff84d19df794c0b4d881f624ce2b74b868312d
André Díaz <andre290800@gmail.com>
2021-10-21 09:45:57 -0500

    First commit.

---
+++ store/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Collection, Product
+from .serializers import CollectionSerializer, ProductSerializer
+
+
+@api_view()
+def product_list(request):
+    queryset = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(
+        queryset, many=True, context={'request': request})
+    return Response(serializer.data)
+
+
+@api_view()
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    collection = get_object_or_404(Collection, pk=pk)
+    serializer = CollectionSerializer(collection)
+    return Response(serializer.data)

5e4c77224256c6a498431c1cd8949797b7034bce
André Díaz <andre290800@gmail.com>
2021-10-25 17:40:23 -0500

    Building the Collections API.

--- store/serializers.py
+++ store/serializers.py
@@ -7,13 +7,18 @@ from store.models import Product, Collection
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
-        fields = ['id', 'title']
+        fields = ['id', 'title', 'products_count']
+    products_count = serializers.SerializerMethodField(
+        method_name='get_products_count')
+
+    products_count = serializers.IntegerField()
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
+        fields = ['id', 'title', 'description', 'slug', 'inventory',
+                  'unit_price', 'price_with_tax', 'collection']
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
 
